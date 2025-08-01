594df11ee99a509cd7bba757742f63d6480943e2
siuupriyanshu <mandalpriyanshu22@gmail.com>
2025-08-01 22:05:17 +0545

    emitting events

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,11 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  })
- });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
 
 server.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
