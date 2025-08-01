6d48d66fe1910eafd33a59aeba9654e46cce654c
siuupriyanshu <mandalpriyanshu22@gmail.com>
2025-08-01 21:53:29 +0545

    integrating socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import express from 'express';
 import type { Application, Request, Response } from 'express';
+import { Server } from "socket.io";
 import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 
 const app: Application  = express();
-
 const server = createServer(app);
+const io = new Server(server);
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(join(__dirname, '../public/index.html'));
 });
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  })
+ });
 
 server.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
